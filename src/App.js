085b56ee64f7059d8d17af72902fb0ea392f0213
Siddhartha Tiwari <siddharthatiwari37@gmail.com>
2024-11-21 22:24:55 +0545

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import "./App.css"
+import "./index.css"
+const App = () => {
 
-function App() {
+  const app = useRef(null);
+  const tl = useRef()
+
+  useEffect(() => {
+
+    let ctx = gsap.context(() => {
+
+      tl.current = gsap.timeline()
+        .to(".square", { rotate: 360 })
+        .to(".square2", { x: 200 });
+
+    }, app);
+
+    return () => ctx.revert()
+
+  }, [])
+
+
+   const message = ["Hello my name is SId","have you herd about it ?","Are you ok?"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div ref={app}>
+      <div className="square">Hello World</div>
+      <div className="square2">Hello World 2</div>
+	<p className = "comic-neue-bold-italic">Hello my name is Siddhartha tiwari.</p>
+	  {
+	     message.map(msg=><div  className = "comic-neue-regular-italic">
+		     {msg}
+		     </div>)
+	  }
 
+	  {
+		  message.map(msg => <div className = "comic-neue-bold">
+			  {msg}
+		  </div>)
+	  }
+
+	  
+    </div>
+
+
+  )
+}
 export default App;
